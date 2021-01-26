466550ee1642ec04c4cf6448839e7383cd38c0b4
Marc Steele <marcsteele2014@gmail.com>
2021-01-26 02:55:50 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
-      t.string :_name
+      t.string :name
       t.integer :actor_id
       t.integer :show_id
     end
